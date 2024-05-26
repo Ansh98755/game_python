22c154a5e4fccc42b2a13eff7d3d36463081a17a
Ansh98755 <ayushmehendiratta3390@gmail.com>
2024-05-26 17:18:43 +0530

    Adding the player to the game

--- ga.py
+++ ga.py
@@ -9,14 +9,18 @@ pygame.display.set_caption("Space Dodge")  # Setting the title of the window
 
 # Loading the background image
 BG = pygame.image.load("s.jpg")
-
+BG=pygame.transform.scale(pygame.image.load("s.jpg"),(1000,800))
+player_width=60
+player_height=50
 # Function to draw elements on the screen
-def draw():
+def draw(player):
     screen.blit(BG, (0, 0))  # Drawing the background image at the top-left corner (0, 0)
+    pygame.draw.rect(screen,"red",player)
     pygame.display.update()  # Updating the display to reflect the changes
 
 # Main function to run the game loop
 def main():
+    player=pygame.Rect(200,800-player_height,player_width,player_height)
     clock = pygame.time.Clock()  # Creating a clock object to control the frame rate
 
     while True:
@@ -25,7 +29,7 @@ def main():
                 pygame.quit()  # Quitting Pygame
                 exit()  # Exiting the program
 
-        draw()  # Calling the draw function to render the background
+        draw(player)  # Calling the draw function to render the background
 
         clock.tick(60)  # Setting the frame rate to 60 frames per second (FPS)
 
